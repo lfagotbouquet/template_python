572e8032928482f86e9c44ffd9427d95b03010ab
Loic Fagot-Bouquet <loic.fagotbouquet@gmail.com>
2024-09-01 16:10:30 +0200

    [DEV] Add pybind installation files

--- install_scripts/install_system_requirements.sh
+++ install_scripts/install_system_requirements.sh
@@ -5,4 +5,7 @@ sudo apt update -y
 # pyenv dependencies
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
 
+# pybind module dependencies
+sudo apt install -y cmake
+
 sudo apt install -y vim
---
+++ tests/test_basic.py
@@ -0,0 +1,6 @@
+import cmake_example as m
+
+
+def test_main():
+    assert m.add(1, 2) == 3
+    assert m.subtract(1, 2) == -1
